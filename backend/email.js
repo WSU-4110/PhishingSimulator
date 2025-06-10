bb4769beace045fe1956b0c7605f78e4ba053b78
Taso01-ops <hc4817@wayne.edu>
2025-06-09 20:30:54 -0400

    Update email.js
    Working Version of sending emails. Still needs further revision for Authentication.

--- backend/email.js
+++ backend/email.js
@@ -1,28 +1,39 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer"); //Create a Transporter
 
-async function main() {
-    // Create a transport. Replace with your own transport options.
-    const transport = nodemailer.createTransport({
-        sendmail: true,
-    });
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: { //we'll be changing this Transporter to something more automated for ease of use.
+        user: "tcollege1524@gmail.com",
+        pass: "lvoo nlpl feej oqvw",
+    },
+});
 
-    const info = await transport.sendMail({
-        from: "Mailer <mailer@example.com>", // Header From:
-        to: "Daemon <daemon@example.com>", // Header To:
-        envelope: {
-            from: "bounce+12345@example.com", // MAIL FROM:
-            to: [
-                // RCPT TO:
-                "daemon@example.com",
-                "mailer@example.com",
-            ],
-        },
-        subject: "Custom SMTP envelope",
-        text: "Hello!",
-    });
+let message = { //Documentation for sending Emails. This sends mail
+    from: "College Student <tcollege1524@gmail.com>", //Student Gmail. Have to work on transporter Authentication for different emails. 
+    to: "tcollege1524@gmail.com", //My student gmail
+    subject: "Example message with GIF",
+    text: "This is the plainText version",
+    html: "<p>For clients that do not support AMP4EMAIL or amp content is not valid</p>", //This sends if AMP (which I hoped to use for cool GIF's) is allowed. Gmail does not. :(
+    amp: `<!doctype html>
+    <html ⚡4email>
+      <head>
+        <meta charset="utf-8">
+        <style amp4email-boilerplate>body{visibility:hidden}</style>
+        <script async src="https://cdn.ampproject.org/v0.js"></script>
+        <script async custom-element="amp-anim" src="https://cdn.ampproject.org/v0/amp-anim-0.1.js"></script>
+      </head>
+      <body>
+        <p>Image: <amp-img src="https://cldup.com/P0b1bUmEet.png" width="16" height="16"/></p>
+        <p>GIF (requires "amp-anim" script in header):<br/>
+          <amp-anim src="https://cldup.com/D72zpdwI-i.gif" width="500" height="350"/></p>
+      </body>
+    </html>`,
+};
 
-    console.log("Envelope used:", info.envelope);
-    // => { from: 'bounce+12345@example.com', to: [ 'daemon@example.com', 'mailer@example.com' ] }
-}
-
-main().catch(console.error);
+transporter.sendMail(message, (err, info) => { //Sends the message
+    if (err) {
+        return console.error("Error", err);
+    } else {
+        console.log("Email sent successfully:", info);
+    }
+});
