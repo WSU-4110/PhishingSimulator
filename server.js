836fe6024e7a179b65cdc8df2a10e625a9535907
taslima-khanom <tasskhan1@gmail.com>
2025-06-10 17:25:37 -0400

    Update server.js // note to add, store and secure video modules directly in our server and database

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
+//must add data for video storage so that the videos are private and secure
 
 app.use(express.json());
 app.use(express.static('public')); // Serve static files
