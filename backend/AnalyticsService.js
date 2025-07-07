03ac3b353c7f329fa06aa35fb070df177952558b
Taso01-ops <hc4817@wayne.edu>
2025-07-07 18:50:50 -0400

    Create AnalyticsService.js
    This allows for AdminDashboard to have analytics

---
+++ backend/AnalyticsService.js
@@ -0,0 +1,35 @@
+const fs = require("fs"); //required file system
+const path = require("path"); //file path
+
+class AnalyticsService {
+  getUserInteractionStats() {
+    const trackingPath = path.join(_dirname, "package.json"); //or this may be a different tracking path.
+    const phishingPath = path.join(_dirname, "data", "phishAttempts.json");
+
+    let trackingData = {}; 
+    let phishingAttempts = [];
+
+    if (fs.existsSync(trackingPath)) {
+      trackingData = JSON.parse(fs.readFileSync(trackingPath,"utf8")); //Read the attempts for email opening
+    }
+
+    if (fs.existsSync(phishingPath)) { //Read the attempts for link clicking 
+      phishingAttempts = JSON.parse(fs.readFileSync(phishingPath,"utf8"));
+    }
+
+    const result = Object.entries(trackingData).map(([token, { email, opened, openedAt }]) => {
+      const clicked = phishingAttempts.some(a => a.username === email);
+      return {
+        email,
+        opened,
+        openedAt,
+        clicked,
+        completedTraining: false 
+      };
+    });
+
+    return result;
+  }
+}
+
+module.exports = AnalyticsService;
