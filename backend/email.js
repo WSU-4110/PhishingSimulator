e54b74e45d1bffcbbca4ca3c1ff056f56e9d0896
Taso01-ops <hc4817@wayne.edu>
2025-07-07 12:18:32 -0400

    Update email.js
    Allowed basic usability for custom subject line and custom email w/ default link included. Will add some gif before demonstration

--- backend/email.js
+++ backend/email.js
@@ -7,7 +7,13 @@ const EmailTemplateFactory = require("./emailTemplates/EmailTemplateFactory");
 
 // Select which phishing email template to use
 const selectedTemplate = "DocShare"; // You can change to "PasswordReset" or "PayStub"
-const emailContent = EmailTemplateFactory.createTemplate(selectedTemplate);
+const customsubjectLine = "Important Document sent by HR for Review"; //Added custom subject line. All of these updates will be updated for usability.
+const defaultLink = "https://givemeyourmoney.edu/doc"; //Added custom link. All of these updates will be updated for usability.
+
+const emailContent = EmailTemplateFactory.createTemplate(selectedTemplate, { //Implemented some email content. All of these updates will be updated for usability.
+  link: defaultLink;
+  subject: customSubject;
+});
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -46,7 +52,7 @@ const message = {
   to: "employee@example.com", // Replace with target test email
   subject: emailContent.subject,
   text: emailContent.plainText,
-  html: emailContent.html,
+  html: htmlWithTracking, //Changed this to include Tracking pixel (1x1).
 };
 
 transporter.sendMail(message, (err, info) => {
