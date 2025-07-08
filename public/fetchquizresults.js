6fb35470dc34609fbe94e5b23dc01de1ccb8da45
Samayyah Cook <samayyahcook@wayne.edu>
2025-07-08 17:24:27 -0400

    fetch quiz results

---
+++ public/fetchquizresults.js
@@ -0,0 +1,12 @@
+fetch('/api/quiz-results', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    employee_id: 1,
+    company_id: 1,
+    module_number: 1,
+    score: '4/4'
+  })
+})
+.then(res => res.text())
+.then(data => alert("Server says: " + data));
